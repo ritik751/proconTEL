2ff10ad0c2ac09d1aa7244239dc3d8f3c278c36a
ritik751 <66561077+ritik751@users.noreply.github.com>
2023-03-15 17:31:20 +0530

    Add files via upload

---
+++ HELLOWORLD/Endpoint.cs
@@ -0,0 +1,25 @@
+﻿using ProconTel.CommunicationCenter.Kernel;
+using ProconTel.Logging;
+
+namespace helloworld_proconTEL
+{
+    [Endpoint(Name = "HelloWorld.Endpoint", SupportedRoles = SupportedRoles.None)]
+
+  public class Demo : ChannelEndpointBase 
+    {
+       
+        public override SubscriberStrategyBase InstantiateSubscriberStrategy(){
+            return null;
+        }
+        public override ProviderStrategyBase InstantiateProviderStrategy(){
+            return null;
+        }
+        public override void Initialize(){
+         Logger.Information("Hello World. proconTEL");
+        }
+
+        public override void Terminate(){
+            Logger.Information("Terminated.");
+        }
+    }
+}
